1952012b0c512042bb157fa3801f548bcca4fc7b
Lonnon Foster <lonnon@rover.com>
2018-12-27 15:46:36 -0800

    Add @git_support decorator to git_space rule

--- config/thefuck/rules/git_space.py
+++ config/thefuck/rules/git_space.py
@@ -1,6 +1,9 @@
 import re
 
+from thefuck.specific.git import git_support
 
+
+@git_support
 def match(command):
     return (
         'command not found: gi' in command.output
@@ -8,6 +11,7 @@ def match(command):
     )
 
 
+@git_support
 def get_new_command(command):
     args = re.search('gi t(.+)', command.script).group(1)
     return 'git {}'.format(args)
