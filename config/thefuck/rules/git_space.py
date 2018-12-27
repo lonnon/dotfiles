adf3258029cfef6af583c3c204af5e6cd9b8c048
Lonnon Foster <lonnon@rover.com>
2018-12-27 15:20:27 -0800

    Add thefuck config and rules to config dir

---
+++ config/thefuck/rules/git_space.py
@@ -0,0 +1,18 @@
+import re
+
+
+def match(command):
+    return (
+        'gi: command not found' in command.output
+        and re.match('gi t', command.script)
+    )
+
+
+def get_new_command(command):
+    args = re.search('gi t(.+)', command.script).group(1)
+    return 'git {}'.format(args)
+
+
+enabled_by_default = True
+priority = 1000
+requires_output = True
